72ba5965374df1d7b731eb54ba87768eb8ec2b6f
Ronaldo Rocha <ronaldo.devlearning@gmail.com>
2025-03-05 09:45:18 -0300

    Commit inicial

---
+++ src/test/java/com/devlearning/algafood/AlgafoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devlearning.algafood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgafoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
